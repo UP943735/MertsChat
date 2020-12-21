f844865a2496896af521c1842ebaca50ed927e0a
MERT <UP943735@myport.ac.uk>
2020-12-21 15:25:11 +0300

    First Commit

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+const chatForm = document.getElementById('chat-form');``
+const chatMessages = document.querySelector('.chat-messages');
+
+//Get username and room from URL
+const{username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+console.log(username, room);
+
+const socket = io();
+
+//Join chatroom
+socket.emit('joinRoom', {username, room});
+
+//Message from Server
+socket.on ('message', message =>{0
+    console.log(message);
+    outputMessage(message);
+
+    //Scroll Down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message Submit
+chatForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    //Get Message Text
+    const msg = e.target.elements.msg.value
+
+    //Emit Message to Server
+    socket.emit('chatMessage', msg);
+
+    //Clear Input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus = '';
+})
+
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div')
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.user}<span>${message.time}</span></p>
+    <p class="text">
+       ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+} 
---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//  Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const botName = 'Merts Bot';
+
+// Run when client connects
+io.on('connection', socket =>{
+    socket.on('joinRoom', ({ username, room })=>{
+
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room)
+
+    //Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to MertsChat!'));
+   
+    //Broadcast when a user connects
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+    //Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage('USER', msg));
+   })
+
+   //Runs when client disconnects
+   socket.on('disconnect', () => {
+   io.emit('message', formatMessage(botName,`${user.username} has left the chat`))    
+   })
+
+}); });
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
